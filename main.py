3dc2798b6fea854d1c7214f60e7d6b9b7148b8cd
Oluwole <oluwoleafolabi0@gmail.com>
2025-01-26 10:54:17 +0100

    refactor: remove db stuff

--- main.py
+++ main.py
@@ -1,40 +1,23 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import Dict, Optional
-import openai
-from pymongo import MongoClient
 from dotenv import load_dotenv
+import openai
 import os
-
-app = FastAPI()
 
 load_dotenv()
 
-mongo_uri = os.getenv("MONGO_URL")
+app = FastAPI()
+
 openai.api_key = os.getenv("API_KEY")
-
-client = MongoClient(mongo_uri)
-
-db = client["case_bud_dev"]
-logs_collection = db["logs"]
 
 class QueryInput(BaseModel):
     query: str
-    user_id: Optional[str] = None 
-
-def log_interaction(query: str, response: str, metadata: Dict):
-    log_entry = {
-        "query": query,
-        "response": response,
-        "metadata": metadata,
-    }
-    logs_collection.insert_one(log_entry)
 
 @app.post("/legal-assistant/")
 async def legal_assistant(query_input: QueryInput):
     try:
         user_query = query_input.query
-        user_id = query_input.user_id
         
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
@@ -45,9 +28,6 @@ async def legal_assistant(query_input: QueryInput):
         )
 
         ai_response = response["choices"][0]["message"]["content"]
-        metadata = {"user_id": user_id} if user_id else {}
-        
-        log_interaction(user_query, ai_response, metadata)
         
         return {"query": user_query, "response": ai_response}
     
@@ -57,4 +37,4 @@ async def legal_assistant(query_input: QueryInput):
 @app.head("/")
 @app.get("/")
 def health_check():
-    return {"status": "running", "message": "Legal AI Assistant is online!"}
+    return {"status": "running", "message": "Legal AI Assistant is online!"}
